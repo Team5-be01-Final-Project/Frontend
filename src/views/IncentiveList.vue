dec25621e99bf9b278b31ab93eb78d3e3c16f1a7
jsmin6330 <jsmin6330@gmail.com>
2024-03-27 11:17:05 +0900

    [Feat] 부서명 검색 조회 기능 추가

--- src/views/IncentiveList.vue
+++ src/views/IncentiveList.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
       <h1>직원 인센티브 목록</h1>
+      <div>
+        <va-select
+          v-model="selectedDepartment"
+          placeholder="부서 선택"
+          :options="deptOptions"
+        />
+        <VaButton @click="applyFilter">검색</VaButton>
+      </div>
       <table>
         <thead>
           <tr>
@@ -12,12 +20,12 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in incentives" :key="item.empName">
+          <tr v-for="item in filteredData" :key="item.empName">
             <td>{{ item.empName }}</td>
-            <td>{{ item.deptCode }}</td>
-            <td>{{ item.voucMonthSales }}</td>
+            <td>{{ item.deptName }}</td>
+            <td class = 'money-right'>{{ formatNumberWithCommas(item.voucMonthSales) }}</td>
             <td>{{ item.salesRank }}</td>
-            <td>{{ item.incentive }}</td>
+            <td class = 'money-right'>{{ formatNumberWithCommas(item.incentive) }}</td>
           </tr>
         </tbody>
       </table>
@@ -26,11 +34,16 @@
   
   <script>
   import axios from 'axios';
+  import { departments } from '../utils/department.js'; 
+  import formatNumberWithCommas from '../utils/formatNumberWithCommas.js';
   
   export default {
     data() {
       return {
-        incentives: []
+        incentives: [],
+        selectedDepartment: '',
+        deptOptions: departments.map(dept => ({ text: dept.text, value: dept.text })),
+        filteredData: [] // 필터링된 데이터를 저장할 배열
       }
     },
     mounted() {
@@ -39,12 +52,22 @@
     methods: {
       async fetchIncentives() {
         try {
-          const response = await axios.get('/incentive/list?month=2');
+          const response = await axios.get('/incentive/list?month=3');
           this.incentives = response.data;
+          this.filteredData = response.data; // 초기 상태에서는 모든 데이터를 표시
         } catch (error) {
           console.error("인센티브 데이터를 가져오는 중 에러가 발생했습니다.", error);
         }
+      },
+      formatNumberWithCommas,
+      applyFilter() {
+      if (!this.selectedDepartment) {
+        this.filteredData = this.incentives;
+      } else {
+        this.filteredData = this.incentives.filter(item => item.deptName === this.selectedDepartment.value);
       }
+    },
+
     }
   }
   </script>
@@ -56,11 +79,16 @@
     border-collapse: collapse;
   }
   
+  td.money-right {
+    text-align: right;
+  }
+
   th, td {
     border: 1px solid black;
     padding: 8px;
     text-align: left;
   }
+
   
   thead {
     background-color: #f2f2f2;
