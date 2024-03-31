9fe310cad3cd053154a7c1c4431ba798ec0e775a
parc02 <pascalmin49@gmail.com>
2024-04-01 03:50:51 +0900

    코드 조금 수정

--- src/components/charts/CarTempBarchart.vue
+++ src/components/charts/CarTempBarchart.vue
@@ -35,15 +35,23 @@ export default {
             if (this.chart) {
                 this.chart.destroy(); // 기존 차트가 있으면 삭제
             }
+            // storageCode를 숫자 부분에 따라 정렬
+            const sortedKeys = Object.keys(data).sort((a, b) => {
+                return parseInt(a.substring(3)) - parseInt(b.substring(3));
+            });
+
+            // 정렬된 키에 따라 온도 값도 정렬
+            const sortedTemps = sortedKeys.map(key => data[key]);
+
             this.chart = new Chart(ctx, {
                 type: 'bar',
                 data: {
-                    labels: Object.keys(data), // 차량 코드를 라벨로 사용
+                    labels: sortedKeys, // 정렬된 차량 코드를 라벨로 사용
                     datasets: [{
                         label: '온도 (°C)',
-                        data: Object.values(data),
-                        backgroundColor: Object.values(data).map(temp => (temp < 2 || temp > 8) ? 'rgba(255, 99, 132, 0.2)' : 'rgba(54, 162, 235, 0.2)'),
-                        borderColor: Object.values(data).map(temp => (temp < 2 || temp > 8) ? 'rgba(255, 99, 132, 1)' : 'rgba(54, 162, 235, 1)'),
+                        data: sortedTemps,
+                        backgroundColor: sortedTemps.map(temp => (temp < 2 || temp > 8) ? 'rgba(255, 99, 132, 0.2)' : 'rgba(54, 162, 235, 0.2)'),
+                        borderColor: sortedTemps.map(temp => (temp < 2 || temp > 8) ? 'rgba(255, 99, 132, 1)' : 'rgba(54, 162, 235, 1)'),
                         borderWidth: 1
                     }]
                 },
@@ -57,7 +65,7 @@ export default {
                     scales: {
                         y: {
                             beginAtZero: true,
-                            min: -5, // y축의 최소값 설정
+                            min: 0, // y축의 최소값 설정
                             max: 10, // y축의 최대값 설정
                             ticks: {
                                 stepSize: 2 // y축의 간격 설정
