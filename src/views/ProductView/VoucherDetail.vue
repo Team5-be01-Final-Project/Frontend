917c36481ae4f6fbfcafff3252a06801cc8cd015
dhkdtld37 <hyeonnls96@naver.com>
2024-04-01 23:39:32 +0900

    [Fix] 승인 버튼 일부 수정

--- src/views/ProductView/VoucherDetail.vue
+++ src/views/ProductView/VoucherDetail.vue
@@ -1,85 +1,101 @@
 <!-- VoucherDetail.vue -->
 <template>
-
-<div class="flex">
+  <div class="flex">
     <!-- 사이드바 섹션 -->
     <div class="sidebar">
-      <ProductSidebar/>
+      <ProductSidebar />
     </div>
 
-  <div>
-    <div class="header">
-      <h3 class="va-h3">출고전표 결재</h3>
-      <div class="button-container" v-if="showApproveButton || showRejectButton">
-        <button v-if="showApproveButton" @click="approveVoucher" class="approve-button">승인</button>
-        <button v-if="showRejectButton" @click="rejectVoucher" class="reject-button">반려</button>
-      </div>
-    </div>
-    <div class="spacer" style="height: 20px"></div>
-    <div class="voucher-info">
-      <div class="voucher-info-row">
-        <div class="voucher-info-item">
-          <span class="voucher-info-label">전표번호:</span>
-          <span class="voucher-info-value">{{ voucId }}</span>
-        </div>
-        <div class="voucher-info-item">
-          <span class="voucher-info-label">등록일:</span>
-          <span class="voucher-info-value">{{ voucDate }}</span>
-        </div>
-        <div class="voucher-info-item">
-          <span class="voucher-info-label">담당자:</span>
-          <span class="voucher-info-value">{{ empName }}</span>
-        </div>
-      </div>
-      <div class="voucher-info-row">
-        <div class="voucher-info-item">
-          <span class="voucher-info-label">결재자:</span>
-          <span class="voucher-info-value">{{ signerName }}</span>
-        </div>
-        <div class="voucher-info-item">
-          <span class="voucher-info-label">거래처:</span>
-          <span class="voucher-info-value">{{ clientName }}</span>
-        </div>
-        <div class="voucher-info-item">
-          <span class="voucher-info-label">차량 번호:</span>
-          <span class="voucher-info-value">{{ storageCar }}</span>
+    <div>
+      <div class="header">
+        <h3 class="va-h3">출고전표 결재</h3>
+        <div
+          class="button-container"
+          v-if="showApproveButton || showRejectButton"
+        >
+          <button
+            v-if="showApproveButton"
+            @click="approveVoucher"
+            class="approve-button"
+          >
+            승인
+          </button>
+          <button
+            v-if="showRejectButton"
+            @click="rejectVoucher"
+            class="reject-button"
+          >
+            반려
+          </button>
         </div>
       </div>
+      <div class="spacer" style="height: 20px"></div>
+      <div class="voucher-info">
+        <div class="voucher-info-row">
+          <div class="voucher-info-item">
+            <span class="voucher-info-label">전표번호:</span>
+            <span class="voucher-info-value">{{ voucId }}</span>
+          </div>
+          <div class="voucher-info-item">
+            <span class="voucher-info-label">등록일:</span>
+            <span class="voucher-info-value">{{ voucDate }}</span>
+          </div>
+          <div class="voucher-info-item">
+            <span class="voucher-info-label">담당자:</span>
+            <span class="voucher-info-value">{{ empName }}</span>
+          </div>
+        </div>
+        <div class="voucher-info-row">
+          <div class="voucher-info-item">
+            <span class="voucher-info-label">결재자:</span>
+            <span class="voucher-info-value">{{ signerName }}</span>
+          </div>
+          <div class="voucher-info-item">
+            <span class="voucher-info-label">거래처:</span>
+            <span class="voucher-info-value">{{ clientName }}</span>
+          </div>
+          <div class="voucher-info-item">
+            <span class="voucher-info-label">차량 번호:</span>
+            <span class="voucher-info-value">{{ storageCar }}</span>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div class="spacer" style="height: 20px"></div>
+      <div v-if="voucherDetails.length > 0">
+        <table class="va-table va-table--hoverable">
+          <thead>
+            <tr>
+              <th style="width: 10%">No.</th>
+              <th style="width: 30%">제품명</th>
+              <th style="width: 15%">수량</th>
+              <th style="width: 20%">판매가</th>
+              <th style="width: 25%">합계</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(detail, index) in voucherDetails" :key="detail.voucId">
+              <td>{{ index + 1 }}</td>
+              <td>{{ detail.proName }}</td>
+              <td>{{ detail.voucAmount }}</td>
+              <td>{{ detail.voucSale.toLocaleString() }}원</td>
+              <td>{{ detail.voucSales.toLocaleString() }}원</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="total-sales">
+          총합계 : {{ totalVoucSales.toLocaleString() }}원
+        </div>
+      </div>
+      <p v-else>해당 전표번호에 대한 정보가 없습니다.</p>
     </div>
-    <hr />
-    <div class="spacer" style="height: 20px"></div>
-    <div v-if="voucherDetails.length > 0">
-      <table class="va-table va-table--hoverable">
-        <thead>
-          <tr>
-            <th style="width: 10%;">No.</th>
-            <th style="width: 30%;">제품명</th>
-            <th style="width: 15%;">수량</th>
-            <th style="width: 20%;">판매가</th>
-            <th style="width: 25%;">합계</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(detail, index) in voucherDetails" :key="detail.voucId">
-            <td>{{ index + 1 }}</td>
-            <td>{{ detail.proName }}</td>
-            <td>{{ detail.voucAmount }}</td>
-            <td>{{ detail.voucSale.toLocaleString() }}원</td>
-            <td>{{ detail.voucSales.toLocaleString() }}원</td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="total-sales">총합계 : {{ totalVoucSales.toLocaleString() }}원</div>
-    </div>
-    <p v-else>해당 전표번호에 대한 정보가 없습니다.</p>
-  </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import VoucherApproval from "@/components/VoucherApproval.vue";
-import ProductSidebar from '@/components/sidebar/ProductSidebar.vue'
+import ProductSidebar from "@/components/sidebar/ProductSidebar.vue";
 
 export default {
   components: { VoucherApproval, ProductSidebar },
@@ -134,24 +150,30 @@ export default {
     },
     async approveVoucher() {
       try {
-        await axios.put(`/api/vouchers/${this.voucId}/approve`, {
-          proCode: this.proCode,
-          empCode: this.empCode,
-        });
+        const response = await axios.put(
+          `/api/vouchers/${this.voucId}/approve`,
+          {
+            proCode: this.proCode,
+            empCodeSign: this.loggedInUserEmpCode,
+          }
+        );
         console.log("Voucher approved");
-        this.fetchVoucherDetails();
+        await this.fetchVoucherDetails();
       } catch (error) {
         console.error("Error approving voucher:", error);
       }
     },
     async rejectVoucher() {
       try {
-        await axios.put(`/api/vouchers/${this.voucId}/reject`, {
-          proCode: this.proCode,
-          empCode: this.empCode,
-        });
+        const response = await axios.put(
+          `/api/vouchers/${this.voucId}/reject`,
+          {
+            proCode: this.proCode,
+            empCodeSign: this.loggedInUserEmpCode,
+          }
+        );
         console.log("Voucher rejected");
-        this.fetchVoucherDetails();
+        await this.fetchVoucherDetails(); // 데이터 갱신
       } catch (error) {
         console.error("Error rejecting voucher:", error);
       }
@@ -188,12 +210,12 @@ export default {
 }
 
 .approve-button {
-  background-color: #4CAF50;
+  background-color: #4caf50;
   color: white;
 }
 
 .reject-button {
-  background-color: #F44336;
+  background-color: #f44336;
   color: white;
 }
 
