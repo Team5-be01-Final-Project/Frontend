6cacc8c1ed9855900e79ac7bba616136ab704d42
parc02 <pascalmin49@gmail.com>
2024-04-04 18:13:09 +0900

    start 또 수정

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
-# 프로젝트 디렉토리로 이동
 cd /home/ubuntu/app/bps
-
-# Docker Compose를 사용하여 서비스 시작
-sudo docker compose -f docker-compose.yml up -d --build --force-recreate
+# Docker Compose를 사용하여 컨테이너를 시작합니다.
+docker-compose -f docker-compose.yml up -d
