36905d8129cdb0634ce62da9ab20933a78f30038
parc02 <pascalmin49@gmail.com>
2024-04-04 17:42:47 +0900

    compose또 수정

--- start.sh
+++ start.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
 cd /home/ubuntu/app/bps
-# Docker Compose를 사용하여 컨테이너를 시작합니다.
-docker-compose -f docker-compose.yml up -d
 
+# 터미널 로그 확인
+set -x
+
+# vue 프로젝트 빌드
+npm install
+npm run build
+
+# 생성된 빌드 파일의 소유자 변경
+sudo chown -R ubuntu:ubuntu dist
+
+# public 디렉토리의 권한 변경
+sudo chmod -R +w public
+
+# ./gradlew bootJar
+
+sudo docker compose -f docker-compose.yml up -d --build --force-recreate
