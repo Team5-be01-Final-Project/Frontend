5f75a74c50e1d97764abeddbbf8f6bcc95c86244
dhkdtld37 <hyeonnls96@naver.com>
2024-04-05 16:28:32 +0900

    [Fix] 로딩 지연 해결 및 조회 완전 성공

--- src/views/MySales.vue
+++ src/views/MySales.vue
@@ -1,44 +1,43 @@
 <!-- MySales.vue -->
 <template>
-    <div class="my-sales-container">
-      <h2>My 영업</h2>
-      <div class="employee-info-container">
-        <h3>사원 정보</h3>
-        <div class="employee-info">
-          <img :src="employee.empImg" alt="사원 사진" />
-          <p>사 원 명 : {{ employee.empName }}</p>
-          <p>사원코드 : {{ employee.empCode }}</p>
-          <p>연 락 처 : {{ employee.empTel }}</p>
-          <p>이 메 일 : {{ employee.empEmail }}</p>
-          <p>직    급 : {{ employee.positions?.posName }}</p>
-          <p>부    서 : {{ employee.department?.deptName }}</p>
-
-        </div>
-      </div>
-      <div class="client-list-container">
-        <h3>담당 거래처 목록</h3>
-        <table>
-          <thead>
-            <tr>
-              <th>거래처명</th>
-              <th>대표자</th>
-              <th>주소</th>
-              <th>담당자</th>
-              <th>전화번호</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="client in clients" :key="client.clientCode">
-              <td>{{ client.clientName }}</td>
-              <td>{{ client.clientBoss }}</td>
-              <td>{{ client.clientWhere }}</td>
-              <td>{{ client.clientEmp }}</td>
-              <td>{{ client.clientEmpTel }}</td>
-            </tr>
-          </tbody>
-        </table>
+  <div class="my-sales-container">
+    <h2>My 영업</h2>
+    <div class="employee-info-container">
+      <h3>사원 정보</h3>
+      <div class="employee-info">
+        <img :src="employee.empImg" alt="사원 사진" />
+        <p>사 원 명 : {{ employee.empName }}</p>
+        <p>사원코드 : {{ employee.empCode }}</p>
+        <p>연 락 처 : {{ employee.empTel }}</p>
+        <p>이 메 일 : {{ employee.empEmail }}</p>
+        <p>직 급 : {{ employee.posName }}</p>
+        <p>부 서 : {{ employee.deptName }}</p>
       </div>
     </div>
+    <div class="client-list-container">
+      <h3>담당 거래처 목록</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>거래처명</th>
+            <th>대표자</th>
+            <th>주소</th>
+            <th>담당자</th>
+            <th>전화번호</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="client in clients" :key="client.clientCode">
+            <td>{{ client.clientName }}</td>
+            <td>{{ client.clientBoss }}</td>
+            <td>{{ client.clientWhere }}</td>
+            <td>{{ client.clientEmp }}</td>
+            <td>{{ client.clientEmpTel }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
   </template>
   
   <script setup>
@@ -51,7 +50,6 @@
   
   onMounted(async () => {
     const empCode = Cookies.get('empCode');
-  
     try {
       // My 영업 정보 조회 API 호출
       const response = await axios.get(`/mysales/${empCode}`);
