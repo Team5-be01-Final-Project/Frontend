d9f260eb6e0469fd5b79882fb85cf0bc53318154
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-04-05 17:40:23 +0900

    [fix] Dockerfile 수정

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 
 cd /home/ubuntu/app/bps
 # Docker Compose를 사용하여 컨테이너를 시작합니다.
-
-sudo docker compose -f docker-compose.yml up -d --build --force-recreate
+docker-compose -f docker-compose.yml up -d
