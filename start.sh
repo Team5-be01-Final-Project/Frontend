175ccc48629d26e15f1a2e38ff6533bf4956691c
parc02 <pascalmin49@gmail.com>
2024-04-05 16:22:14 +0900

    [Fix]코드 수정

--- start.sh
+++ start.sh
@@ -2,4 +2,9 @@
 
 cd /home/ubuntu/app/bps
 # Docker Compose를 사용하여 컨테이너를 시작합니다.
-docker-compose -f docker-compose.yml up -d
+
+# vue 프로젝트 빌드
+npm run build
+
+
+sudo docker compose -f docker-compose.yml up -d --build --force-recreate
