1af9a92824d0060ebc8fdf32e3c2465a2cc5e9db
jsmin6330 <jsmin6330@gmail.com>
2024-04-06 14:32:28 +0900

    [Fix] 도커 컴포즈 명령어 변경

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home/ubuntu/app/bps
-# Docker Compose를 사용하여 컨테이너를 시작합니다.
-docker-compose -f docker-compose.yml up -d
+
+docker-compose -f docker-compose.yml up -d --build --force-recreate
